6479d6407ba26e36cb3599652dc59149efcd6ce8
Sladan Mastilovic <mastilovic@yahoo.com>
2020-10-02 19:36:04 -0700

    2

--- index.js
+++ index.js
@@ -5,18 +5,47 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search For a Movie</b></label>
+<input class="input"/>
+<div class = "dropdown">
+<div class="dropdown-menu">
+<div class="dropdown-content results"></div>
+</div>
+</div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+    const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+    <img src="${imgSRC}" />
+    ${movie.Title}
+    `;
+    resultsWrapper.appendChild(option);
   }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 1000);
 };
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput, 600));
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
